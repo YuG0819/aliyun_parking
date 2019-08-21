b6689478e848b01f067f126fb27659a7aa5086c3
Warzonee <1603229751@qq.com>
2019-08-21 11:50:51 +0800

    update models

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=20)
 # Create your models here.
+
+class test(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(null=False,max_length=20)
